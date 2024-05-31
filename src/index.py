961c1dde394f5c6b91b1ddca3211beaf155ad2b4
mxdkwu <148736007+mxdkwu@users.noreply.github.com>
2024-05-31 10:28:54 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,42 +1,39 @@
 from fastapi import FastAPI, Request, HTTPException
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage
+import json
+import time
 
 app = FastAPI()
 
-# LINE Bot 相關設定
-access_token = os.getenv('LINE_ACCESS_TOKEN')
-channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+access_token = '你的 Access Token'
+channel_secret = '你的 Channel Secret'
+
 line_bot_api = LineBotApi(access_token)
 handler = WebhookHandler(channel_secret)
 
-# LINE Bot webhook 處理
-@app.post("/callback")
-async def callback(request: Request):
-    signature = request.headers["X-Line-Signature"]
-    body = await request.body()
+@app.post("/")
+async def linebot(request: Request):
+    body = await request.body()                      # 取得收到的訊息內容
     try:
-        handler.handle(body.decode(), signature)
-    except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="Missing Parameters")
-    return "OK"
-
-# 處理收到的訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handling_message(event):
-    if isinstance(event.message, TextMessage):
-        user_message = event.message.text
-        # 這裡可以根據用戶訊息做出回應
-        reply_msg = f"你說了：{user_message}"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
-
-# 預設路由
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+        signature = request.headers['X-Line-Signature']   # 加入回傳的 headers
+        handler.handle(body.decode(), signature)      # 綁定訊息回傳的相關資訊
+        json_data = json.loads(body)         # 轉換內容為 json 格式
+        reply_token = json_data['events'][0]['replyToken']    # 取得回傳訊息的 Token ( reply message 使用 )
+        user_id = json_data['events'][0]['source']['userId']  # 取得使用者 ID ( push message 使用 )
+        print(json_data)                                      # 印出內容
+        type = json_data['events'][0]['message']['type']
+        if type == 'text':
+            text = json_data['events'][0]['message']['text']
+            if text == '雷達回波圖' or text == '雷達回波':
+                line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))   # 一開始先發送訊息
+                img_url = f'https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-001.png?{time.time_ns()}'
+                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
+                line_bot_api.reply_message(reply_token, img_message)  # 回傳訊息
+            else:          
+                text_message = TextSendMessage(text=text)
+                line_bot_api.reply_message(reply_token,text_message)
+    except Exception as e:
+        print(e)                # 發生錯誤就印出完整錯誤內容
+    return 'OK'                 # 驗證 Webhook 使用，不能省略

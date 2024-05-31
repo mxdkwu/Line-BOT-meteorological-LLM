cb8b7aa2ee82ebc10f7624868f82f23970490caf
mxdkwu <148736007+mxdkwu@users.noreply.github.com>
2024-05-31 10:26:24 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,78 +1,42 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import PlainTextResponse
-import json, time, requests
-import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage
-import uvicorn
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
 app = FastAPI()
 
-def earth_quake():
-    result = []
-    code = os.getenv('CWA_TOKEN')  # 從環境變數獲取中央氣象局 API 授權碼
-    try:
-        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
-        req1 = requests.get(url)
-        data1 = req1.json()
-        eq1 = data1['records']['Earthquake'][0]
-        t1 = data1['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
-
-        url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
-        req2 = requests.get(url2)
-        data2 = req2.json()
-        eq2 = data2['records']['Earthquake'][0]
-        t2 = data2['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
-        
-        result = [eq1['ReportContent'], eq1['ReportImageURI']]
-        if t2 > t1:
-            result = [eq2['ReportContent'], eq2['ReportImageURI']]
-    except Exception as e:
-        print(e)
-        result = ['抓取失敗...', '']
-    return result
-
+# LINE Bot 相關設定
 access_token = os.getenv('LINE_ACCESS_TOKEN')
 channel_secret = os.getenv('LINE_CHANNEL_SECRET')
 line_bot_api = LineBotApi(access_token)
 handler = WebhookHandler(channel_secret)
 
-@app.post("/")
-async def linebot(request: Request):
+# LINE Bot webhook 處理
+@app.post("/callback")
+async def callback(request: Request):
+    signature = request.headers["X-Line-Signature"]
     body = await request.body()
-    signature = request.headers['x-line-signature']
-
     try:
-        handler.handle(body.decode('utf-8'), signature)
-        json_data = json.loads(body.decode('utf-8'))
-        reply_token = json_data['events'][0]['replyToken']
-        user_id = json_data['events'][0]['source']['userId']
-        print(json_data)
-        message_type = json_data['events'][0]['message']['type']
-
-        if message_type == 'text':
-            text = json_data['events'][0]['message']['text']
-            if text == '雷達回波圖' or text == '雷達回波':
-                await line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
-                img_url = f'https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-001.png?{time.time_ns()}'
-                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
-                await line_bot_api.reply_message(reply_token, img_message)
-            elif text == '地震':
-                await line_bot_api.push_message(user_id, TextSendMessage(text='馬上找給你！抓取資料中....'))
-                reply = earth_quake()
-                text_message = TextSendMessage(text=reply[0])
-                await line_bot_api.reply_message(reply_token, text_message)
-                await line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
-            else:
-                text_message = TextSendMessage(text=text)
-                await line_bot_api.reply_message(reply_token, text_message)
+        handler.handle(body.decode(), signature)
     except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="無效的簽名")
-    except Exception as e:
-        print(e)
+        raise HTTPException(status_code=400, detail="Missing Parameters")
+    return "OK"
 
-    return PlainTextResponse('OK')
+# 處理收到的訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handling_message(event):
+    if isinstance(event.message, TextMessage):
+        user_message = event.message.text
+        # 這裡可以根據用戶訊息做出回應
+        reply_msg = f"你說了：{user_message}"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# 預設路由
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
